d5250c87e15f342ff304dfd19d8e3a322ad5af00
Diego Lara <dyegolara@gmail.com>
2016-03-17 14:15:28 -0600

    Patrones de diseño, php y tvify - gatos terminado

---
+++ patrones/BuildPatterns.js
@@ -0,0 +1,43 @@
+function Director() {
+    "use strict";
+    this.construct = function (builder) {
+        builder.step1();
+        builder.step2();
+        return builder.getResult();
+    };
+}
+
+function Car() {
+    "use strict";
+    this.doors = 0;
+    this.addParts = function () {
+        this.doors = 4;
+    };
+    this.doSomething = function () {
+        console.log("tengo " + this.doors + " puertas");
+    };
+}
+
+function CarBuilder() {
+    "use strict";
+    this.car = null;
+    this.step1 = function () {
+        this.car = new Car();
+    };
+    this.step2 = function () {
+        this.car.addParts();
+    };
+    this.getResult = function () {
+        return this.car;
+    };
+}
+
+function testBuildPattern() {
+    "use strict";
+    var shop = new Director(),
+        carBuilder = new CarBuilder(),
+        car = shop.construct(carBuilder);
+    car.doSomething();
+}
+
+testBuildPattern();
